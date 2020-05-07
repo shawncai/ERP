7d5047a59815558269481fe5c95cdc2ca29fba1a
beyond <931414026@qq.com>
2020-05-07 15:01:28 +0800

    beyond 5.7.5

--- src/directive/permission2/permission2.js
+++ src/directive/permission2/permission2.js
@@ -12,8 +12,8 @@ export default{
       const hasPermission = roles.some(role => {
         return permissionRoles.includes(role)
       })
-      // console.log('hasPermission', hasPermission)
-      // console.log('value[1]', value[1])
+      console.log('hasPermission', hasPermission)
+      console.log('value[1]', value[1])
       if (!hasPermission || value[1] !== store.getters.userId) {
         el.parentNode && el.parentNode.removeChild(el)
       }
